14fa55d1366297ad9a3eb1f0e285f06555c420dd
ro-prg <ro.digraff@gmail.com>
2023-06-06 22:54:46 -0400

    formulario

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const Formulario = () => {
+  const [nombre, setNombre] = useState('');
+  const [apellido, setApellido] = useState('');
+  const [telefono, setTelefono] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (nombre === '' || apellido === '' || telefono === '' || email === '') {
+      alert('Por favor, completa todos los campos');
+    } else {
+      // Aquí puedes agregar la lógica para enviar los datos del formulario
+      console.log('Nombre:', nombre);
+      console.log('Apellido:', apellido);
+      console.log('Teléfono:', telefono);
+      console.log('Email:', email);
+
+      // Restablecer los valores del formulario
+      setNombre('');
+      setApellido('');
+      setTelefono('');
+      setEmail('');
+    }
+  };
+
+  const handleTelefonoChange = (e) => {
+    const regex = /^[0-9\b]+$/; // Expresión regular que solo permite números
+
+    if (e.target.value === '' || regex.test(e.target.value)) {
+      setTelefono(e.target.value);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Nombre:
+        <input
+          type="text"
+          value={nombre}
+          onChange={(e) => setNombre(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Apellido:
+        <input
+          type="text"
+          value={apellido}
+          onChange={(e) => setApellido(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Teléfono:
+        <input
+          type="tel"
+          value={telefono}
+          onChange={handleTelefonoChange}
+        />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <br />
+      <button type="submit">Enviar</button>
+    </form>
   );
-}
+};
 
-export default App;
+export default Formulario;
